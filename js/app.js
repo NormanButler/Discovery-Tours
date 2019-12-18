8333d839d4060640c85ed99926bbe5002331532c
Norman Butler <normanabutler@hotmail.com>
2019-12-18 22:35:54 +0000

    Fixed Hamburger Menu and other formatting

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 // $(document).ready(function() {
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
 
 // });
+
+
+$(document).ready() 
+
+	$('#hamburger').on('click', function() {
+		$('nav').slideToggle();
+	});
+
+	$(window).on('resize', function() {
+		if ($(window).width() >= 1000 && !$('nav').is(':visible')) {
+			$('nav').show();
+		} else if ($(window).width() < 1000 && $('nav').is(':visible')) {
+			$('nav').hide();
+		}
+	});
